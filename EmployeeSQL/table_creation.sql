4d989300ec63ea4362b6792797199339c0113ff8
m-janssens-boop <127706155+m-janssens-boop@users.noreply.github.com>
2023-05-12 13:10:45 -0700

    table creation file complete

--- EmployeeSQL/table_creation.sql
+++ EmployeeSQL/table_creation.sql
@@ -6,3 +6,13 @@ CREATE TABLE dept_empt
 
 CREATE TABLE dept_manager
 (dept_no VARCHAR(30), emp_no INT);
+
+CREATE TABLE employees
+(emp_no INT, emp_title_id VARCHAR(30), birth_date DATE, 
+ first_name VARCHAR(50), last_name VARCHAR(50), sex VARCHAR(30), hire_date DATE);
+ 
+CREATE TABLE salaries
+(emp_no INT, salary INT);
+
+CREATE TABLE dept_empt
+(title_id VARCHAR(30), title VARCHAR(50));
